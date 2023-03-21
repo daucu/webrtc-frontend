428249b896faa19b5d782eb694a940ee145731b2
Dauqu <sujeetsingh734321@gmail.com>
2023-03-21 12:18:48 +0530

    hfjfj

--- src/Context.js
+++ src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:4000');
+const socket = io('https://webrtc-backend-ccg5py.dauqu.host');
 // const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
